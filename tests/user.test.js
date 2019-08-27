708b6ad2458f6dbe720b1acc86a0cca97dfc57d4
Peter Sims <ragtimesims@gmail.com>
2019-08-26 22:06:51 -0400

    first test case

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,13 @@
 const request = require('supertest');
+const app = require('../src/app');
+
+test('Should signup a new user', async () => {
+	await request(app)
+		.post('/users')
+		.send({
+			name: 'P Sims',
+			email: 'a@adfsaf.com',
+			password: 'Peter123'
+		})
+		.expect(201);
+});
